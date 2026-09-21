363f1f9e5f98c6a2df02385149e86c6550f54b23
Vishal Kumar <vishalrathour2003@gmail.com>
2026-09-21 22:19:29 +0530

    added summarize.py

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
 from utlis.audio_processor import process_input
 from core.transcriber import transcribe_all
 import os
 
+
+
 os.environ["PATH"] += os.pathsep + r"C:\Users\desktop\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-9.0.1-full_build\bin"
 
-source = "https://www.youtube.com/watch?v=l-QPwk_f4eE"
+source = "https://www.youtube.com/watch?v=vFP1mgZ_LEY"
+language = "hinglish"
 
 chunks = process_input(source)
-print(transcribe_all(chunks))
+transcript = transcribe_all(chunks,language=language)
+print("\n=== TRANSCRIPT ===\n ")
+print(transcript)
